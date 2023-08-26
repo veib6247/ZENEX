2010bb6a4b6740075548059b19f84d5e7c2a3c0c
Bryan Olandres <ozoneblacklight@outlook.com>
2023-08-26 15:33:21 +0800

    implementing zd api endpoints

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import logging
+import os
 
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # init logging
 logging.basicConfig(
@@ -12,14 +16,31 @@ logging.basicConfig(
 subdomain = 'payretosupport'
 domain = 'zendesk.com'
 url = f'https://{subdomain}.{domain}'
+endpoint_search = '/api/v2/search.json'  # GET
+endpoint_tickets = '/api/v2/tickets'  # GET
 
 
 def main():
+    zd_user_email = os.getenv('ZD_EMAIL')
+    zd_token = os.getenv('ZD_TOKEN')
+    query_params = {'query': 'type:ticket status:pending'}
+
     try:
-        response = requests.get(url)
+        response = requests.get(
+            f'{url}{endpoint_tickets}',
+            auth=(f'{zd_user_email}/token', zd_token),
+            params=query_params
+        )
+
         match response.status_code:
             case 200:
-                logging.info(response.text)
+                payload = dict(response.json())
+                ticket_list = payload.get('tickets')
+
+                for ticket in ticket_list:
+                    logging.info(ticket)
+
+                logging.info(f'Displayed {len(ticket_list)} tickets')
 
             case _:
                 logging.error(f'got http: {response.status_code}')
