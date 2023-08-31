26f6ffaa99cab10acf401c05878acbe5264e3ef8
Bryan Olandres <ozoneblacklight@outlook.com>
2023-08-31 19:20:17 +0800

    added docstrings and sample calls

--- main.py
+++ main.py
@@ -27,7 +27,11 @@ class Zenex:
         return f'https://{self.subdomain}.{self.domain}'
 
     def search_tickets(self, query_params: str):
-        # https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/
+        '''
+        Returns the 1st page of the query.
+
+        https://developer.zendesk.com/api-reference/ticketing/ticket-management/search
+        '''
 
         try:
             res = requests.get(
@@ -73,8 +77,13 @@ class Zenex:
             exit()
 
     def get_exportable_tickets(self, query_params: str):
-        # https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#export-search-results
-        # "This endpoint is for search queries that will return more than 1000 results. The result set is ordered only by the created_at attribute."
+        '''
+        Return 1st page of the query using the export endpoint.
+
+        "This endpoint is for search queries that will return more than 1000 results. The result set is ordered only by the created_at attribute."
+
+        https://developer.zendesk.com/api-reference/ticketing/ticket-management/search/#export-search-results
+        '''
 
         try:
             res = requests.get(
@@ -119,38 +128,9 @@ class Zenex:
             logging.exception(e)
             exit()
 
-        # https://developer.zendesk.com/api-reference/ticketing/groups/groups/#show-group
-
-        try:
-            res = requests.get(
-                f'{self.get_url()}/api/v2/groups/{group_id}.json',
-                auth=(f'{self.zd_user_email}/token', self.zd_token)
-            )
-
-            match res.status_code:
-                case 200:
-                    payload = dict(res.json())
-
-                    if self.enable_logging:
-                        logging.info(json.dumps(payload, indent=4))
-
-                    else:
-                        return payload
-
-                case _:
-                    if self.enable_logging:
-                        logging.error(f'HTTP {res.status_code} - {res.reason}')
-                        logging.error(res.text)
-
-                    else:
-                        return {'error': res.text}
-
-        except Exception as e:
-            logging.exception(e)
-            exit()
-
     def get_id_context(self, resource: str, id: str):
         '''Take in a resource and the ID. Valid resources are users, ticket_fields, brands, organizations, groups.'''
+
         try:
             res = requests.get(
                 f'{self.get_url()}/api/v2/{resource}/{id}.json',
@@ -185,6 +165,7 @@ def main():
     zd_user_email = os.getenv('ZD_EMAIL')
     zd_token = os.getenv('ZD_TOKEN')
 
+    # instantiate class
     zd = Zenex(
         domain='zendesk.com',
         subdomain='payretosupport',
@@ -200,13 +181,10 @@ def main():
     # with open('downloads/exportable.json', 'w') as file:
     #     file.write(json.dumps(exportable))
 
-    # zd.show_user('901178672543')
-    # zd.show_field('16862464937369')
-    # zd.show_brand('900000066203')
-    # zd.show_organization('900001325246')
-    # zd.show_group('900000372563')
-
     zd.get_id_context(resource='users', id='900120880703')
+    zd.get_id_context(resource='ticket_fields', id='16862464937369')
+    zd.get_id_context(resource='brands', id='900000066203')
+    zd.get_id_context(resource='organizations', id='900001325246')
 
 
 if __name__ == '__main__':
